616dc8dcdee99c423c9f9c0076c6862370f6882f
xpole <newyeardreamer@gmail.com>
2022-05-24 17:23:52 +0300

    Работа с числами

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int i:intList) {
+            if (i > 0 && i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
