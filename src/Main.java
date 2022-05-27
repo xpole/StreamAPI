303ad1e347e4b7715f9c116ae923be3488473f36
xpole <newyeardreamer@gmail.com>
2022-05-27 16:29:05 +0300

    StreamAPI

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         Collections.sort(intList);
-        for (int i:intList) {
+        for (int i : intList) {
             if (i > 0 && i % 2 == 0) {
                 System.out.println(i);
             }
